260a76f5b7f11ed8d7094cf41a3727fbc907c728
Kushal Hada <kushaldeveloper@gmail.com>
2021-03-27 14:52:55 -0400

    add all files

---
+++ readme.markdown
@@ -0,0 +1,10 @@
+# Learning and using playwright
+
+Thank you for joining me here. 
+
+The original log is at [/logs/hello.txt](/logs/hello.txt).
+
+This is an educational project. 
+It is not production ready. 
+
+Sincerely,
---
+++ src/test.ts
@@ -0,0 +1,90 @@
+import { Page } from "playwright";
+
+const sentinel = "https://staplespromo.com";
+const visited: string[] = [];
+const threshold_in_seconds = 2;
+const max_capacity = 1000000;
+
+const { chromium } = require('playwright');
+(async () => {
+  const browser = await chromium.launch({
+    headless: true,
+    args: ['--window-size=2560,1440']
+  });
+  const context = await browser.newContext({
+    viewport: {
+      width: 1920,
+      height: 1080,
+    }
+  });
+  // Open new page
+  const page = await context.newPage();
+  await page.goto(sentinel);
+  // Click text=I consent to cookies 
+  await page.click('text=I consent to cookies');
+  // ---------------------
+
+  await benchmark(sentinel, context);
+
+  await context.close();
+  await browser.close();
+})();
+
+async function benchmark(address: string, context: { newPage: () => any; }) {
+  try {
+    let hrefs: string[];
+    const page = await context.newPage();
+    const start_time = +new Date();
+    console.log(`Now visiting ${address}`);
+    await page.goto(address);
+    const metrics = await getMetrics(page);
+    let safeAddress = address.replace(/[^A-Z0-9]/gi, "");
+    if (safeAddress.length > 25) {
+      safeAddress = `${safeAddress.substring(15,30)}-${start_time}`;
+    }
+    console.log({ safeAddress });
+    page.screenshot({
+      path: `screenshots/${safeAddress}.png`,
+      fullPage: true,
+    });
+    const end_time = +new Date();
+    const load_time = (end_time - start_time) / 1000;
+    if (metrics.data.duration > threshold_in_seconds * 1000) {
+      console.log({ metrics });
+      console.log(`Total load time for ${address} was ${load_time} seconds`);
+    }
+    hrefs = await page.$$eval("a", (as: any[]) => as.map((a) => a.href));
+    for (const href of hrefs) {
+      const hrefHashPosition = href.indexOf("#");
+      const hrefWithoutHash =
+        hrefHashPosition > -1 ? href.substr(0, hrefHashPosition) : href;
+      if (
+        !visited.includes(hrefWithoutHash) &&
+        href.startsWith(sentinel) &&
+        visited.length < max_capacity
+      ) {
+        visited.push(hrefWithoutHash);
+        await benchmark(hrefWithoutHash, context);
+      }
+    }
+  } catch (error) {
+    console.log(`Error benchmarking ${address}`);
+    console.error({ error });
+  }
+}
+
+async function getMetrics(
+  page: Page
+): Promise<{
+  format: "PerformanceNavigationTiming";
+  data: PerformanceNavigationTiming;
+}> {
+  return JSON.parse(
+    await page.evaluate(() => {
+      const [timing] = performance.getEntriesByType("navigation");
+      return JSON.stringify({
+        data: timing,
+      });
+    })
+  );
+}
