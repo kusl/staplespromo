252ffcad5b3a534e33ce8ee16685b1cc48de6058
Kushal Hada <kushaldeveloper@gmail.com>
2021-03-27 14:55:21 -0400

    limit myself to 720 pages

--- src/test.ts
+++ src/test.ts
@@ -3,7 +3,7 @@ import { Page } from "playwright";
 const sentinel = "https://staplespromo.com";
 const visited: string[] = [];
 const threshold_in_seconds = 2;
-const max_capacity = 1000000;
+const max_capacity = 720;
 
 const { chromium } = require('playwright');
 (async () => {
